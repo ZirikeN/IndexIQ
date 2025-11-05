9d700bb6b7baf08c133ebac6ae47e8b7ee994946
Dmitriy <orbetfair1@gmail.com>
2025-11-05 13:38:50 +0300

    first commit

---
+++ app/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>    
+        <h1 class="text-3xl font-bold text-blue-500">Тест Tailwind</h1>
+        <UButton to="/products">Тест Nuxt UI Button</UButton>
+    </div>
+</template>
